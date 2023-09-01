71e81ff970a6483bbc01a144826de3dcaadfd4cb
j-mahapatra <jagannathm.official@gmail.com>
2023-09-01 21:44:12 +0530

    Created basic routes and Navbar component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 function App() {
   return (
-    <>
-      <h1>Chatbox</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
